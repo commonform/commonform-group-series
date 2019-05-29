43d70494aa4614fff180ff0bfe9599365947437f
Kyle E. Mitchell <kyle@kemitchell.com>
2019-05-28 22:07:05 -0700

    npx standard --fix

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ var predicate = require('commonform-predicate')
 
 module.exports = function commonformGroupSeries (form) {
   return form.content
-  .reduce(function (groups, element, index, content) {
-    if (isChild(element)) {
+    .reduce(function (groups, element, index, content) {
+      if (isChild(element)) {
       // `element` is part of the previous series.
-      if (index > 0 && isChild(content[index - 1])) {
-        groups[groups.length - 1].content.push(element)
-      // `element` starts a new series.
+        if (index > 0 && isChild(content[index - 1])) {
+          groups[groups.length - 1].content.push(element)
+          // `element` starts a new series.
+        } else {
+          groups.push({
+            type: 'series',
+            content: [element]
+          })
+        }
       } else {
-        groups.push({
-          type: 'series',
-          content: [element]
-        })
-      }
-    } else {
       // `element` is part of the previous paragraph.
-      if (index > 0 && !isChild(content[index - 1])) {
-        groups[groups.length - 1].content.push(element)
-      // `element` starts a new paragraph.
-      } else {
-        groups.push({
-          type: 'paragraph',
-          content: [element]
-        })
+        if (index > 0 && !isChild(content[index - 1])) {
+          groups[groups.length - 1].content.push(element)
+          // `element` starts a new paragraph.
+        } else {
+          groups.push({
+            type: 'paragraph',
+            content: [element]
+          })
+        }
       }
-    }
-    return groups
-  }, [])
+      return groups
+    }, [])
 }
 
 function isChild (argument) {
